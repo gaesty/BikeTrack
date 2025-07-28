3e8bed8f55c88c1eae79ef98ac299cc678400775
Gaëtan <gaetan.styskal@gmail.com>
2025-07-28 08:46:40 +0200

    Update arduino_secrets.h

--- CodeFonctionnel/arduino_secrets.h
+++ CodeFonctionnel/arduino_secrets.h
@@ -8,7 +8,7 @@
 
 // Code PIN de votre carte SIM (4 chiffres généralement)
 // IMPORTANT: Changez cette valeur selon votre carte SIM réelle
-#define SECRET_SIM_PIN_CODE "2305"
+#define SECRET_SIM_PIN_CODE {PIN_SIM_CARD}
 
 // ============================================================================
 // CONFIGURATION RÉSEAU MOBILE - Paramètres APN
@@ -19,7 +19,7 @@
 // Orange: "orange.fr" 
 // SFR: "websfr"
 // Free: "free"
-#define SECRET_APN "mmsbouygtel.com"
+#define SECRET_APN {YOUR_APN}
 
 // Identifiants GPRS (généralement vides pour les opérateurs français)
 #define SECRET_GPRS_USER ""  // Nom d'utilisateur (laissez vide si non requis)
@@ -32,7 +32,7 @@
 // Identifiant unique de ce dispositif BikeTrack
 // Format recommandé: [MODÈLE]_[NUMÉRO] (ex: A7670E_001, A7670E_002...)
 // Cet ID permet de différencier plusieurs trackers dans la base de données
-#define SECRET_DEVICE_ID "A7670E_003"
+#define SECRET_DEVICE_ID {NAME_DEVICE}
 
 // ============================================================================
 // CONFIGURATION SERVEUR - Paramètres de connexion au proxy BikeTrack
@@ -40,10 +40,10 @@
 
 // URL complète du serveur proxy BikeTrack
 // Ce serveur fait le pont entre le dispositif et la base de données Supabase
-#define SECRET_PROXY_URL "http://35.193.109.50:3000/proxy"
+#define SECRET_PROXY_URL "http://{IP_ADRESS}:3000/proxy"
 
 // Paramètres de connexion décomposés (utilisés par le client TCP)
-#define PROXY_HOST "35.193.109.50"  // Adresse IP ou nom d'hôte du serveur
+#define PROXY_HOST {IP_ADRESS}  // Adresse IP ou nom d'hôte du serveur
 #define PROXY_PORT 3000             // Port TCP du serveur (généralement 3000)
 
 #endif // ARDUINO_SECRETS_H
